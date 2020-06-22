f73cdbc93c3cc4fa5abfc4c6939789cbc85346e1
Michael Alls <michael.alls@fedex.com>
2020-06-22 09:51:48 -0400

    module 2 Refactoring the Library

--- src/cli.js
+++ src/cli.js
@@ -1,40 +1,35 @@
-var index = require("./index.js");
-var LineEndings = index.LineEndings;
-var transformLineEnding = index.transformLineEnding;
-
-var helpText = require("./info.js");
-var help = helpText.help;
-var usage = helpText.usage;
+const { LineEndings, transformLineEnding } = require('./index.js');
+const { help, usage } = require('./info.js');
 
 // Display help text and exit if when someone passes `-h` or `--help`.
-if (process.argv.includes("-h") || process.argv.includes("--help")) {
+if (process.argv.includes('-h') || process.argv.includes('--help')) {
   console.log(help);
   process.exit(0);
 }
 
 // Gather input from stdin.
-var buffer = "";
+let buffer = '';
 
-process.stdin.on("data", data => {
+process.stdin.on('data', data => {
   buffer += data;
 });
 
 // When stdin closes, operate on the gathered input.
-process.stdin.on("end", () => {
-  var results;
+process.stdin.on('end', () => {
+  let results;
 
   switch (process.argv.slice(-1)[0]) {
-    case "CR":
+    case 'CR':
       results = transformLineEnding(buffer, LineEndings.CR);
       process.stdout.write(results);
       break;
 
-    case "LF":
+    case 'LF':
       results = transformLineEnding(buffer, LineEndings.LF);
       process.stdout.write(results);
       break;
 
-    case "CRLF":
+    case 'CRLF':
       results = transformLineEnding(buffer, LineEndings.CRLF);
       process.stdout.write(results);
       break;
